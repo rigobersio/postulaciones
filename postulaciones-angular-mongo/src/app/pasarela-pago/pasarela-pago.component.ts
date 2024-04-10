1457afcfdc73c7969f3017b405ee9da95ccbb97f
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-04-09 21:49:51 -0400

    (ryzen)New:pasarelaPago()

---
+++ postulaciones-angular-mongo/src/app/pasarela-pago/pasarela-pago.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pasarela-pago',
+  standalone: true,
+  imports: [],
+  templateUrl: './pasarela-pago.component.html',
+  styleUrl: './pasarela-pago.component.css'
+})
+export class PasarelaPagoComponent {
+
+}
